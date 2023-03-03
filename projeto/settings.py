1dc8e3a681ca04bd62548ffe2b4a6e47ebbac227
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2023-03-03 13:16:35 -0300

    Update project

--- projeto/settings.py
+++ projeto/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -135,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 
 # Login
 
